20ad67e58494c4d9c21e3188aad8499e77952421
Owner <Owner@My-PC>
2020-08-09 02:27:43 -0500

    Still trying to take over the spawn events for entities

--- src/main/java/net/mysticcloud/spigot/survival/listeners/PlayerListener.java
+++ src/main/java/net/mysticcloud/spigot/survival/listeners/PlayerListener.java
@@ -1,11 +1,14 @@
 package net.mysticcloud.spigot.survival.listeners;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
 
 import net.mysticcloud.spigot.core.utils.CoreUtils;
+import net.mysticcloud.spigot.core.utils.MysticPlayer;
 import net.mysticcloud.spigot.survival.MysticSurvival;
 
 public class PlayerListener implements Listener {
@@ -21,6 +24,19 @@ public class PlayerListener implements Listener {
 					+ e.getEntity().getName().substring(1, e.getEntity().getName().length()).toLowerCase();
 			e.getEntity().setCustomName(CoreUtils.colorize("&8" + name));
 			e.getEntity().setCustomNameVisible(true);
+			MysticPlayer player = null;
+			for(Entity entity : e.getEntity().getNearbyEntities(50, 50, 50)) {
+				if(entity instanceof Player) {
+					player = CoreUtils.getMysticPlayer(entity.getUniqueId());
+					break;
+				}
+			}
+			if(player == null) {
+				e.setCancelled(true);
+				return;
+			}
+			e.getEntity().setCustomName(CoreUtils.colorize("&7[" + player.getLevel() + "] &2&l" + name));
+			
 		}
 	}
 
