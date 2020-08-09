f0b83b98ed014b7f5ef7683e367603f5e902df07
Owner <Owner@My-PC>
2020-08-09 02:24:43 -0500

    Test

--- src/main/java/net/mysticcloud/spigot/survival/listeners/PlayerListener.java
+++ src/main/java/net/mysticcloud/spigot/survival/listeners/PlayerListener.java
@@ -9,16 +9,18 @@ import net.mysticcloud.spigot.core.utils.CoreUtils;
 import net.mysticcloud.spigot.survival.MysticSurvival;
 
 public class PlayerListener implements Listener {
-	
+
 	public PlayerListener(MysticSurvival plugin) {
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
-	
+
 	@EventHandler
 	public void onEntitySpawn(EntitySpawnEvent e) {
-		if(e.getEntity() instanceof Monster) {
-			String name = e.getEntity().getName().substring(0,1).toUpperCase() + e.getEntity().getName().substring(1,e.getEntity().getName().length()).toLowerCase();
+		if (e.getEntity() instanceof Monster) {
+			String name = e.getEntity().getName().substring(0, 1).toUpperCase()
+					+ e.getEntity().getName().substring(1, e.getEntity().getName().length()).toLowerCase();
 			e.getEntity().setCustomName(CoreUtils.colorize("&8" + name));
+			e.getEntity().setCustomNameVisible(true);
 		}
 	}
 
