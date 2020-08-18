3477f7e82bd66d8b7a465e477dd16ed3c72f5084
Owner <Owner@My-PC>
2020-08-18 18:07:13 -0500

    sdf

--- src/main/java/net/mysticcloud/spigot/survival/utils/SurvivalUtils.java
+++ src/main/java/net/mysticcloud/spigot/survival/utils/SurvivalUtils.java
@@ -17,6 +17,7 @@ public class SurvivalUtils {
 	private static MysticSurvival plugin;
 
 	static Map<Tier, Material[]> weaponTiers = new HashMap<>();
+	static String[] descriptors = new String[] {"Xelphor's", "Shiny", "Swift", "Dull", "Chipped", "Hardy", "Sharp", "Hellish"};
 
 	public static void start(MysticSurvival main) {
 		plugin = main;
@@ -43,23 +44,38 @@ public class SurvivalUtils {
 	public static ItemStack weaponGenerator(int level) {
 
 		boolean mystic = false;
-
+		String name = descriptors[CoreUtils.getRandom().nextInt(descriptors.length)] + " ";
+		
 		Tier tier = Tier.getTier(level);
 		ItemStack item = new ItemStack(
 				weaponTiers.get(tier)[CoreUtils.getRandom().nextInt(weaponTiers.get(tier).length)]);
+		if(item.getType().name().contains("SWORD"))
+			name = name + "Sword";
+		if(item.getType().name().contains("_AXE"))
+			name = name + "Axe";
+		if(item.getType().name().equalsIgnoreCase("TRIDENT"))
+			name = name + "Trident";
+		if(item.getType().name().equalsIgnoreCase("BOW"))
+			name = name + "Bow";
+		if(item.getType().name().equalsIgnoreCase("CROSSBOW"))
+			name = name + "Cross Bow";
 		ItemMeta a = item.getItemMeta();
 		List<String> lore = a.hasLore() ? a.getLore() : new ArrayList<String>();
 		if (CoreUtils.getRandom().nextBoolean() && !mystic) {
 			
-			lore.add(CoreUtils.colorize("&cFire&f Damage: &c&l" + ((int) ( level * (1 / CoreUtils.getRandom().nextInt(4)) )) + "&f"));
+			lore.add(CoreUtils.colorize("&cFire&7 Damage: &c&l" + ((int) ( level * (1 / CoreUtils.getRandom().nextInt(4)) )) + "&7"));
+			name = name + " of &cFlame&f";
 		}
 		if (CoreUtils.getRandom().nextBoolean() && !mystic) {
-			lore.add(CoreUtils.colorize("&bFrost&f Damage: &b&l" + ((int) ( level * (1 / CoreUtils.getRandom().nextInt(4)) )) + "&f"));
+			lore.add(CoreUtils.colorize("&bFrost&7 Damage: &b&l" + ((int) ( level * (1 / CoreUtils.getRandom().nextInt(4)) )) + "&7"));
+			name = name + " of &cFlame&f";
 		}
 		
 		a.setLore(lore);
-		
+		a.setDisplayName(name);
 		item.setItemMeta(a);
+		
+		
 
 		return item;
 	}
