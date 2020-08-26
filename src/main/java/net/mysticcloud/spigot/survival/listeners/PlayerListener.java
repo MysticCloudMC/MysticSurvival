29ac6ef68eb94fe9ce2a171db98c18e26dee286e
Owner <Owner@My-PC>
2020-08-26 10:59:57 -0500

    tf

--- src/main/java/net/mysticcloud/spigot/survival/listeners/PlayerListener.java
+++ src/main/java/net/mysticcloud/spigot/survival/listeners/PlayerListener.java
@@ -15,6 +15,7 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -112,6 +113,7 @@ public class PlayerListener implements Listener {
 			if(player.getStamina() > 10) {
 				player.useStamina(10);
 			} else {
+				e.setUseItemInHand(Result.DENY);
 				e.setCancelled(true);
 			}
 		}
