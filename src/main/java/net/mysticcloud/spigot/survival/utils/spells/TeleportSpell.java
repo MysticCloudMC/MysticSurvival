02b84c0eb6413816f05be9c7bef215d580dbc20e
Owner <Owner@My-PC>
2020-08-27 00:50:00 -0500

    omg

--- src/main/java/net/mysticcloud/spigot/survival/utils/spells/TeleportSpell.java
+++ src/main/java/net/mysticcloud/spigot/survival/utils/spells/TeleportSpell.java
@@ -1,7 +1,11 @@
 package net.mysticcloud.spigot.survival.utils.spells;
 
 import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.entity.LivingEntity;
+
+import net.mysticcloud.spigot.core.utils.particles.ParticleFormat;
+import net.mysticcloud.spigot.core.utils.particles.formats.RandomFormat;
 
 public class TeleportSpell extends Spell {
 	
@@ -13,6 +17,12 @@ public class TeleportSpell extends Spell {
 	
 	@Override
 	public void activate() {
+		ParticleFormat format = new RandomFormat();
+		format.particle(Particle.CLOUD);
+		for(int i=0;i!=15;i++) {
+			format.display(entity.getLocation(), i);
+			format.display(loc, i);
+		}
 		entity.teleport(loc);
 	}
 
