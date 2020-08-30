f0887c35d947cd4a3c55f26b25ca848baff5eb39
Owner <Owner@My-PC>
2020-08-29 20:20:28 -0500

    better graphics & pos

--- src/main/java/net/mysticcloud/spigot/survival/utils/spells/FlameSpell.java
+++ src/main/java/net/mysticcloud/spigot/survival/utils/spells/FlameSpell.java
@@ -15,14 +15,14 @@ public class FlameSpell extends Spell {
 	@Override
 	public void activate() {
 		for (int i = 0; i != 20; i++) {
-			entity.getWorld().spawnParticle(Particle.FLAME, entity.getEyeLocation(), 0,
+			entity.getWorld().spawnParticle(Particle.FLAME, entity.getEyeLocation().add(0, -0.75, 0), 0,
 					entity.getEyeLocation().getDirection().getX()
 							+ ((CoreUtils.getRandom().nextDouble() * (CoreUtils.getRandom().nextBoolean() ? -1 : 1))/8),
 					entity.getEyeLocation().getDirection().getY()
 							+ ((CoreUtils.getRandom().nextDouble() * (CoreUtils.getRandom().nextBoolean() ? -1 : 1))/8),
 					entity.getEyeLocation().getDirection().getZ()
 							+ ((CoreUtils.getRandom().nextDouble() * (CoreUtils.getRandom().nextBoolean() ? -1 : 1))/8),
-					CoreUtils.getRandom().nextDouble());
+					CoreUtils.getRandom().nextDouble()/2);
 		}
 	}
 
