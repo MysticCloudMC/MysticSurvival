f038120e3a2a59ef64a1a998b481ddcf19a7f4ab
Owner <Owner@My-PC>
2020-08-29 19:09:22 -0500

    this should randomize it a lil

--- src/main/java/net/mysticcloud/spigot/survival/utils/spells/FlameSpell.java
+++ src/main/java/net/mysticcloud/spigot/survival/utils/spells/FlameSpell.java
@@ -3,6 +3,8 @@ package net.mysticcloud.spigot.survival.utils.spells;
 import org.bukkit.Particle;
 import org.bukkit.entity.LivingEntity;
 
+import net.mysticcloud.spigot.core.utils.CoreUtils;
+
 public class FlameSpell extends Spell {
 
 	public FlameSpell(LivingEntity entity) {
@@ -11,9 +13,11 @@ public class FlameSpell extends Spell {
 
 	@Override
 	public void activate() {
-		entity.getWorld().spawnParticle(Particle.FLAME, entity.getEyeLocation(), 0,
-				entity.getEyeLocation().getDirection().getX(), entity.getEyeLocation().getDirection().getY(),
-				entity.getEyeLocation().getDirection().getZ(),2);
+		for (int i = 0; i != 20; i++)
+			entity.getWorld().spawnParticle(Particle.FLAME, entity.getEyeLocation(), 0,
+					entity.getEyeLocation().getDirection().getX() + CoreUtils.getRandom().nextDouble(),
+					entity.getEyeLocation().getDirection().getY() + CoreUtils.getRandom().nextDouble(),
+					entity.getEyeLocation().getDirection().getZ() + CoreUtils.getRandom().nextDouble(), 1);
 	}
 
 }
