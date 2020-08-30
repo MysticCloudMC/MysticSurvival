3ea459721657e861b9537f7991d07bd7b6f9c055
Owner <Owner@My-PC>
2020-08-29 19:15:02 -0500

    what's this look like?

--- src/main/java/net/mysticcloud/spigot/survival/utils/spells/FlameSpell.java
+++ src/main/java/net/mysticcloud/spigot/survival/utils/spells/FlameSpell.java
@@ -2,6 +2,7 @@ package net.mysticcloud.spigot.survival.utils.spells;
 
 import org.bukkit.Particle;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.util.Vector;
 
 import net.mysticcloud.spigot.core.utils.CoreUtils;
 
@@ -13,11 +14,17 @@ public class FlameSpell extends Spell {
 
 	@Override
 	public void activate() {
-		for (int i = 0; i != 20; i++)
+		Vector vec = entity.getEyeLocation().getDirection();
+		for (int i = 0; i != 20; i++) {
 			entity.getWorld().spawnParticle(Particle.FLAME, entity.getEyeLocation(), 0,
-					entity.getEyeLocation().getDirection().getX() + CoreUtils.getRandom().nextDouble(),
-					entity.getEyeLocation().getDirection().getY() + CoreUtils.getRandom().nextDouble(),
-					entity.getEyeLocation().getDirection().getZ() + CoreUtils.getRandom().nextDouble(), 1);
+					entity.getEyeLocation().getDirection().getX()
+							+ (CoreUtils.getRandom().nextDouble() * (CoreUtils.getRandom().nextBoolean() ? -1 : 1)),
+					entity.getEyeLocation().getDirection().getY()
+							+ (CoreUtils.getRandom().nextDouble() * (CoreUtils.getRandom().nextBoolean() ? -1 : 1)),
+					entity.getEyeLocation().getDirection().getZ()
+							+ (CoreUtils.getRandom().nextDouble() * (CoreUtils.getRandom().nextBoolean() ? -1 : 1)),
+					1 + CoreUtils.getRandom().nextDouble());
+		}
 	}
 
 }
