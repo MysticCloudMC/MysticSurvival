08cb143503779c5643c45afc075dd65824126aaf
Owner <Owner@My-PC>
2020-08-30 21:23:26 -0500

    bed

--- src/main/java/net/mysticcloud/spigot/survival/runnables/SeekerArrowRunnable.java
+++ src/main/java/net/mysticcloud/spigot/survival/runnables/SeekerArrowRunnable.java
@@ -20,10 +20,10 @@ public class SeekerArrowRunnable implements Runnable {
 	@Override
 	public void run() {
 		if (!arrow.isDead()) {
-			arrow.setVelocity(
-					new Vector(1/Math.sqrt(Math.pow(target.getLocation().getX() - arrow.getLocation().getX(), 2)),
-							1/Math.sqrt(Math.pow(target.getLocation().getY() - arrow.getLocation().getY(), 2)),
-							1/Math.sqrt(Math.pow(target.getLocation().getZ() - arrow.getLocation().getZ(), 2))));
+
+			arrow.setVelocity(new Vector(((arrow.getLocation().getX() - target.getLocation().getX())),
+					((arrow.getLocation().getY() - target.getLocation().getY())),
+					((arrow.getLocation().getZ() - target.getLocation().getZ()))));
 			Bukkit.getScheduler().runTaskLater(SurvivalUtils.getPlugin(), this, 1);
 		}
 
