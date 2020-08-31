465785e92207cb2ef3ecb1433a1d4d0ed3a308f9
Owner <Owner@My-PC>
2020-08-31 18:27:20 -0500

    lol

--- src/main/java/net/mysticcloud/spigot/survival/utils/perks/ArcheryPerkSeeker.java
+++ src/main/java/net/mysticcloud/spigot/survival/utils/perks/ArcheryPerkSeeker.java
@@ -1,9 +1,12 @@
 package net.mysticcloud.spigot.survival.utils.perks;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -20,6 +23,25 @@ public class ArcheryPerkSeeker extends ArcheryPerk {
 		ItemMeta sm = seeker.getItemMeta();
 		sm.setDisplayName(CoreUtils.colorize("&eSeeker Arrow"));
 		seeker.setItemMeta(sm);
+		ready = false;
+	}
+	
+	@Override
+	public void setTarget(LivingEntity target) {
+		this.target = target;
+		ready = true;
+	}
+
+	@Override
+	public String[] getRequirements() {
+		List<String> req = new ArrayList<>();
+		if (target == null) {
+			req.add("Target (LivingEntity)");
+		}
+		String str[] = new String[req.size()];
+		for (int j = 0; j < req.size(); j++)
+			str[j] = req.get(j);
+		return str;
 	}
 	
 	@Override
