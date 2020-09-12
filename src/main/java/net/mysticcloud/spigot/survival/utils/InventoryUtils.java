4320fdc642f1c8a82737dd1f217cbf70a2f602c0
Owner <Owner@My-PC>
2020-09-11 22:14:15 -0500

    ok lol

--- src/main/java/net/mysticcloud/spigot/survival/utils/InventoryUtils.java
+++ src/main/java/net/mysticcloud/spigot/survival/utils/InventoryUtils.java
@@ -1,8 +1,9 @@
 package net.mysticcloud.spigot.survival.utils;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
@@ -10,6 +11,7 @@ import org.bukkit.inventory.ItemStack;
 
 import net.mysticcloud.spigot.core.utils.GUIManager;
 import net.mysticcloud.spigot.core.utils.InventoryCreator;
+import net.mysticcloud.spigot.survival.utils.perks.Perks;
 
 public class InventoryUtils {
 
@@ -31,18 +33,24 @@ public class InventoryUtils {
 		bench = benchInv.getInventory();
 
 		InventoryCreator menuInv = new InventoryCreator("&3&lMenu", null, 54);
-		menuInv.addItem(new ItemStack(Material.GRAY_STAINED_GLASS_PANE), " ", 'X');
+		
+		menuInv.addItem(new ItemStack(Material.GRAY_STAINED_GLASS_PANE), "&7Click an option", 'X');
+		
 		menuInv.addItem(new ItemStack(Material.CRAFTING_TABLE), "&e&lCrafting", 'A', new String[] {
 				"&7- Craft custom items", "&7- Enhance your weapons and armor", "&7- Create spells and potions" });
+		
 		menuInv.addItem(new ItemStack(Material.NETHER_STAR), "&5&lPerks", 'B',
 				new String[] { "&7- Access the perk menus" });
-		menuInv.addItem(new ItemStack(Material.BLACK_CONCRETE), "&c&lComing soon", 'C',
+		
+		
+		menuInv.addItem(new ItemStack(Material.BLACK_CONCRETE), "&c&lComing soon", 'Z',
 				new String[] { "&7- &o???" });
+		
 		menuInv.addItem(new ItemStack(Material.AIR), 'O');
 
-		menuInv.setConfiguration(new char[] { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'A', 'X', 'B', 'X', 'C',
-				'X', 'C', 'X', 'X', 'X', 'C', 'X', 'C', 'X', 'C', 'X', 'X', 'X', 'C', 'X', 'C', 'X', 'C', 'X', 'C', 'X',
-				'X', 'X', 'C', 'X', 'C', 'X', 'C', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' });
+		menuInv.setConfiguration(new char[] { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'A', 'X', 'B', 'X', 'Z',
+				'X', 'Z', 'X', 'X', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'X', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X',
+				'X', 'X', 'Z', 'X', 'Z', 'X', 'Z', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' });
 
 		menu = menuInv.getInventory();
 	}
@@ -58,7 +66,32 @@ public class InventoryUtils {
 		if (GUIManager.getOpenInventory(player).equals("none"))
 			GUIManager.openInventory(player, menu, "Menu");
 		else
-			GUIManager.switchInventory(player, bench, "Menu");
+			GUIManager.switchInventory(player, menu, "Menu");
+	}
+	
+	public static void openPerksMenu(Player player) {
+		
+		InventoryCreator perks = new InventoryCreator("&5&lPerks", null, 9);
+		
+		
+		ArrayList<Character> conf = new ArrayList<>();
+		
+		perks.addItem(new ItemStack(Material.RED_STAINED_GLASS_PANE), "&c&lComing Soon", 'X');
+		
+		for(int i=0;i!=9;i++) {
+			try {
+				perks.addItem(new ItemStack(Division.values()[i].getGUIItem()), Division.values()[i].getDisplayName(), (char)i);
+				conf.add((char)i);
+			} catch(NullPointerException ex) {
+				conf.add('X');
+			}
+		}
+		perks.setConfiguration(conf);
+		
+		if (GUIManager.getOpenInventory(player).equals("none"))
+			GUIManager.openInventory(player, perks.getInventory(), "Perks");
+		else
+			GUIManager.switchInventory(player, perks.getInventory(), "Perks");
 	}
 
 	public static void craft(Inventory inv) {
@@ -76,13 +109,9 @@ public class InventoryUtils {
 	private static ItemStack getResult(LinkedList<ItemStack> items) {
 		ItemStack result = new ItemStack(Material.AIR);
 		if (items.get(4).getType().equals(Material.BOOK)) {
-			Bukkit.broadcastMessage("Item #4 is a book");
 			for (ItemStack item : items) {
-				Bukkit.broadcastMessage("Item..." + item.getType().name());
 				if (!item.getType().equals(Material.BOOK) && !item.getType().equals(Material.AIR)) {
-					Bukkit.broadcastMessage("Found another item: " + item.getType().name());
 					result = SurvivalUtils.enhanceInInventory(item, items.get(4));
-					Bukkit.broadcastMessage("Got result aborting loop.");
 					break;
 				}
 			}
