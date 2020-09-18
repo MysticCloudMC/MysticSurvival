c82aa8aa7a6d231a9c9631fb676c950aebbaee06
Owner <Owner@My-PC>
2020-09-17 22:26:42 -0500

    I wanna work on boids this better work

--- src/main/java/net/mysticcloud/spigot/survival/listeners/PlayerAttackListener.java
+++ src/main/java/net/mysticcloud/spigot/survival/listeners/PlayerAttackListener.java
@@ -112,7 +112,7 @@ public class PlayerAttackListener implements Listener {
 				net.mysticcloud.spigot.survival.utils.items.Item i = new net.mysticcloud.spigot.survival.utils.items.Item(
 						((Player) e.getDamager()).getEquipment().getItemInMainHand());
 				Player player = (Player) e.getDamager();
-				if (ItemUtils.getWeaponType(((Player) e.getDamager()).getEquipment().getItemInMainHand().getType())
+				if (!ItemUtils.getWeaponType(((Player) e.getDamager()).getEquipment().getItemInMainHand().getType())
 						.equals("Stick")) {
 					Weapon weapon = new Weapon(((Player) e.getDamager()).getEquipment().getItemInMainHand());
 					if (SurvivalUtils.getSurvivalPlayer(player).getStamina() >= weapon.getWeight()) {
